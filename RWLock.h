c0cd2bda13848a2d9ada97cf2b28895bf7c4bb0f
Marc Page <marcallenpage@gmail.com>
2011-09-06 11:58:07 -0500

    Cleaning up RWLock and adding a test.

--- RWLock.h
+++ RWLock.h
@@ -5,8 +5,19 @@
 #include <pthread.h>
 #include "Exception.h"
 
+#ifndef trace_scope
+	#define trace_scope ///< in case Tracer.h is not included
+#endif
+#ifndef trace_bool
+	#define trace_bool(x) (x) ///< in case Tracer.h is not included
+#endif
+
 namespace exec {
 
+	/**
+		@todo Document
+		@todo Improve test to detect r/w conflict
+	*/
 	class RWLock {
 	public:
 		enum ReadWrite {
@@ -33,28 +44,28 @@ namespace exec {
 	};
 
 	inline RWLock::Locker::Locker(RWLock &lock, ReadWrite read)
-		:_lock(lock) {
+		:_lock(lock) {trace_scope;
 		_lock.lock(read);
 	}
-	inline RWLock::Locker::~Locker() {
+	inline RWLock::Locker::~Locker() {trace_scope;
 		_lock.unlock();
 	}
 	inline RWLock::RWLock()
-		:_lock() {
+		:_lock() {trace_scope;
 		AssertCodeMessageException(pthread_rwlock_init(&_lock, NULL));
 	}
-	inline RWLock::~RWLock() {
+	inline RWLock::~RWLock() {trace_scope;
 		int		error= pthread_rwlock_destroy(&_lock);
 		void	*__unused__[]= {&error, &__unused__};
 	}
-	inline void RWLock::lock(ReadWrite read) {
+	inline void RWLock::lock(ReadWrite read) {trace_scope;
 		if(Read == read) {
 			AssertCodeMessageException(pthread_rwlock_rdlock(&_lock));
 		} else {
 			AssertCodeMessageException(pthread_rwlock_wrlock(&_lock));
 		}
 	}
-	inline void RWLock::unlock() {
+	inline void RWLock::unlock() {trace_scope;
 		AssertCodeMessageException(pthread_rwlock_unlock(&_lock));
 	}
 
