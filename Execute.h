7ed3bb53a34fe2ee59917ca5cce6592d38ea9c8e
Marc Page <MarcAllenPage@gMail.com>
2012-03-01 12:28:44 -0600

    adding an execute shell command getting stdout command

---
+++ Execute.h
@@ -0,0 +1,41 @@
+#include <string>
+#include <stdio.h>
+#include "Exception.h"
+
+#ifndef trace_scope
+	#define trace_scope ///< in case Tracer.h is not included
+#endif
+#ifndef trace_bool
+	#define trace_bool(x) (x) ///< in case Tracer.h is not included
+#endif
+
+namespace exec {
+	/**
+		@todo Document
+		@todo Test
+	*/
+	inline std::string &execute(const std::string &command, std::string &stdout, const size_t blocks= 4096) {trace_scope
+		FILE	*out= ::popen(command.c_str(), "r");
+
+		errnoNULLAssertMessageException(out);
+		try {
+			stdout.clear();
+			while(!::feof(out)) {
+				std::string::size_type	offset= stdout.size();
+				off_t					amount;
+
+				stdout.append(blocks, '\0');
+				amount= ::fread(const_cast<char*>(&stdout.data()[offset]), 1, blocks, out);
+				if(static_cast<size_t>(amount) != blocks) {
+					stdout.erase(offset + amount);
+					AssertMessageException(ferror(out) == 0);
+				}
+			}
+		} catch(const std::exception&) {trace_scope
+			::pclose(out);
+			throw;
+		}
+		::pclose(out);
+		return stdout;
+	}
+}
---
+++ tests/Execute_test.cpp
@@ -0,0 +1,12 @@
+#include "os/Execute.h"
+#include <stdio.h>
+
+int main(const int argc,const char*const argv[]) {
+	std::string	result;
+
+	for(int arg= 1; arg < argc; ++arg) {
+		exec::execute(argv[arg], result);
+		printf("[[%s]]\n", result.c_str());
+	}
+	return 0;
+}
