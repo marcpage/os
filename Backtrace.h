e8b4388e1a250db061b90f5a1801b83fa22ae622
Marc <marc.page@ni.com>
2019-11-14 15:10:33 -0600

    Improving backtrace output and adding a test for backtrace

--- Backtrace.h
+++ Backtrace.h
@@ -10,20 +10,68 @@
 #include <vector>
 #include <execinfo.h>
 #include <stdlib.h>
+#include <cxxabi.h> // abi::__cxa_demangle
 
 namespace trace {
 
 	typedef std::string String;
 	typedef std::vector<String> StringList;
 
+	/*
+
+		0   Backtrace_clang++_trace             0x000000010c8783f0 _ZN5trace5stackERNSt3__16vectorINS0_12basic_stringIcNS0_11char_traitsIcEENS0_9allocatorIcEEEENS5_IS7_EEEEi + 112
+	*/
+	inline std::string demangleName(const std::string &name) {
+		size_t		dataSize = 0;
+		int			status = 0;
+		std::string	result;
+
+		const char *unmangled = abi::__cxa_demangle(name.c_str(), nullptr, &dataSize, &status);
+
+		if (0 == status) {
+			result = unmangled;
+		} else {
+			result = name;
+		}
+		::free(reinterpret_cast<void*>(const_cast<char*>(unmangled)));
+		return result;
+	}
+	inline std::string demangleLine(const std::string &line) {
+		std::string::size_type start, end;
+		end = line.rfind('+');
+		if ( (std::string::npos == end) || (end < 3) || (' ' != line[end - 1])) {
+			return line;
+		}
+
+		end -= 1;
+		start = line.rfind(' ', end - 1);
+		if ( (std::string::npos == end) || (start < 3) || (end - start < 1) ) {
+			return line;
+		}
+
+		++start;
+		return line.substr(0, start) + demangleName(line.substr(start, end - start)) + line.substr(end);
+	}
+	/*
+		0   Backtrace_clang++_trace             0x000000010c8783f0 _ZN5trace5stackERNSt3__16vectorINS0_12basic_stringIcNS0_11char_traitsIcEENS0_9allocatorIcEEEENS5_IS7_EEEEi + 112
+	*/
+	inline std::string stripPrefix(const std::string &line) {
+		std::string::size_type zeroEx = line.find("0x");
+
+		if (std::string::npos != zeroEx) {
+			return line.substr(zeroEx + 2);
+		}
+		return line;
+	}
 	inline StringList &stack(StringList &list, int maxDepth=4096) {
+		const int	skipStackFrames = 4; // trace::stack() call, std::string, and Exception x 2
 		void	**frames= new void*[maxDepth];
 		int		count= ::backtrace(frames, maxDepth);
 		char	**names= ::backtrace_symbols(frames, count);
 
 		list.clear();
-		for (int i= 0; i < count; ++i) {
-			list.push_back(names[i]);
+		for (int i= skipStackFrames; i < count; ++i) {
+			list.push_back(demangleLine(stripPrefix(names[i])));
 		}
 		::free(names);
 		delete[] frames;
---
+++ tests/Backtrace_test.cpp
@@ -0,0 +1,19 @@
+#include "os/Backtrace.h"
+#include "os/Exception.h"
+#include <stdio.h>
+
+int main(const int , const char * const[]) {
+	int	iterations= 40;
+#ifdef __Tracer_h__
+	iterations= 1;
+#endif
+	for (int i = 0; i < iterations; ++i) {
+		try {
+			ThrowMessageException("Testing");
+		} catch(const std::exception &e) {
+			printf("%s\n", e.what());
+		}
+	}
+	return 0;
+}
+
