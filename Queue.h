54367ffced14cde26cceaeb12d7dd8eb15eeb14c
Marc Page <marcallenpage@gmail.com>
2020-04-14 20:40:13 -0500

    fixing timeout for timeouts less than 1 seconds1

--- Queue.h
+++ Queue.h
@@ -145,8 +145,8 @@ inline bool Queue<T>::dequeue(T &value, double timeoutInSeconds) {
 
   while ((_queue.size() == 0) && (-1 != _max)) {
     if (!_empty.wait_for(lock,
-                         std::chrono::nanoseconds(uint64_t(timeoutInSeconds) *
-                                                  1000 * 1000 * 1000))) {
+                         std::chrono::nanoseconds(uint64_t(timeoutInSeconds *
+                                                  1000 * 1000 * 1000)))) {
       return false;
     } else {
       AssertQueueNotClosed;
