aaf235d4a2e386151272876969f65a2969ec3c1e
Marc Page <marcallenpage@gmail.com>
2020-04-14 21:03:21 -0500

    Fixing timeout

--- Queue.h
+++ Queue.h
@@ -144,9 +144,8 @@ inline bool Queue<T>::dequeue(T &value, double timeoutInSeconds) {
   std::unique_lock<std::mutex> lock(_lock);
 
   while ((_queue.size() == 0) && (-1 != _max)) {
-    if (!_empty.wait_for(lock,
-                         std::chrono::nanoseconds(uint64_t(timeoutInSeconds *
-                                                  1000 * 1000 * 1000)))) {
+    if (!_empty.wait_for(lock, std::chrono::nanoseconds(uint64_t(
+                                   timeoutInSeconds * 1000 * 1000 * 1000)))) {
       return false;
     } else {
       AssertQueueNotClosed;
