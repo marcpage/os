e1e3df339aa3936df9dd0c4a59499f6ccc89b260
Marc Page <marcallenpage@gmail.com>
2020-04-23 10:32:13 -0500

    Updating documentation

--- Queue.h
+++ Queue.h
@@ -7,6 +7,7 @@
 #include <mutex>
 #include <vector>
 
+/// Helper macro that will throw an exception before accessing a closed queue
 #define AssertQueueNotClosed                                                   \
   if (-1 == _max) {                                                            \
     throw Closed("Queue Already Closed", __FILE__, __LINE__);                  \
@@ -15,12 +16,10 @@
 
 namespace exec {
 
-/**
-        @todo Document
-        @todo Test
-*/
+/// A thread-safe queue of a given type
 template <class T> class Queue {
 public:
+  /// Exception that is thrown when attempting to operate on a closed queue
   class Closed : public msg::Exception {
   public:
     /// Get a message
@@ -34,24 +33,70 @@ public:
     /// destructs _message
     virtual ~Closed() throw();
   };
+  /** Create a new queue.
+        @param max The maximum number of items in the queue. If enqueue is
+     attempted, it blocks until there is room. default 0 which means there is no
+     limit.
+        @param reserve The guess at how many items will be in the queue at peak.
+     default 0 which means no space reserved.
+  */
   Queue(int max = 0, int reserve = 0);
+  /// Destructor
   ~Queue();
+  /** Is the queue empty.
+        @return true if there are no elements in the queue.
+  */
   bool empty();
+  /** Is the queue full.
+        @return true if any more elements added to the queue would block.
+        @throws Closed if the queue has been closed
+  */
   bool full();
+  /** The number of elements in the queue.
+        @return The count of all elements in the queue.
+        @throws Closed if the queue has been closed
+  */
   int size();
+  /** Puts an element into the queue.
+        This call will block if the queue is full until an element is dequeued.
+        @param value The value to add to the queue.
+        @return a reference to this queue
+        @throws Closed if the queue has been closed
+  */
   Queue &enqueue(const T &value);
+  /** Puts an element into the queue.
+        This call will block if the queue is full until an element is dequeued.
+        @param value The value to add to the queue.
+        @param timeoutInSeconds The number of seconds to wait if the queue is
+     full.
+        @return true if the element was successfully added, false if the
+     operation timed out
+        @throws Closed if the queue has been closed
+  */
   bool enqueue(const T &value, double timeoutInSeconds);
+  /** Retrieves that oldest value in the queue.
+        @return The oldest value in the queue
+        @throws Closed if the queue has been closed
+  */
   T dequeue();
+  /** Retrieves that oldest value in the queue.
+        @param value receives the value from the queue
+        @param timeoutInSeconds The number of seconds to wait if the queue is
+     empty.
+        @return The oldest value in the queue
+        @throws Closed if the queue has been closed
+  */
   bool dequeue(T &value, double timeoutInSeconds);
+  /// Closes the queue, preventing further operations on the queue.
   void close();
 
 private:
-  typedef std::vector<T> List;
-  std::mutex _lock;
-  std::condition_variable _full;
-  std::condition_variable _empty;
-  List _queue;
-  int _max;
+  typedef std::vector<T> List;     ///< A list of elements
+  std::mutex _lock;                ///< Protection on _queue
+  std::condition_variable _full;   ///< Used to wait if the queue is full
+  std::condition_variable _empty;  ///< Used to wait if the queue is empty
+  List _queue;                     ///< The queue of elements
+  int _max;                        ///< The maximum number of elements
   Queue(const Queue &);            ///< prevent usage
   Queue &operator=(const Queue &); ///< prevent usage
 };
@@ -108,6 +153,9 @@ template <class T> inline Queue<T> &Queue<T>::enqueue(const T &value) {
   _empty.notify_all();
   return *this;
 }
+/**
+        @todo decrement timeoutInSeconds for contention on enqueues
+*/
 template <class T>
 inline bool Queue<T>::enqueue(const T &value, double timeoutInSeconds) {
   std::unique_lock<std::mutex> lock(_lock);
@@ -139,6 +187,9 @@ template <class T> inline T Queue<T>::dequeue() {
   _full.notify_all();
   return value;
 }
+/**
+        @todo decrement timeoutInSeconds for contention on enqueues
+*/
 template <class T>
 inline bool Queue<T>::dequeue(T &value, double timeoutInSeconds) {
   std::unique_lock<std::mutex> lock(_lock);
@@ -162,6 +213,9 @@ template <class T> inline void Queue<T>::close() {
   _empty.notify_all();
   _full.notify_all();
 }
+
+#undef AssertQueueNotClosed // this macro is not valid outside the scope of this
+                            // file
 
 }; // namespace exec
 
